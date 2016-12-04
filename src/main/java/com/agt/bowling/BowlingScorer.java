a15f9887ec8d7e48e1da834d68a2fe32b21033fc
Eli Segal <esegal@Administrators-MacBook-Pro-2.local>
2016-12-04 10:59:35 +0200

    Initial commit

---
+++ src/main/java/com/agt/bowling/BowlingScorer.java
@@ -0,0 +1,12 @@
+package com.agt.bowling;
+
+public class BowlingScorer {
+
+    public void roll(int pins) {
+
+    }
+
+    public int score() {
+        return 0;
+    }
+}
---
+++ src/test/java/test/com/agt/bowling/BowlingScorerTest.java
@@ -0,0 +1,15 @@
+package test.com.agt.bowling;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class BowlingScorerTest {
+
+
+    @Test
+    public void nothing() throws Exception {
+        assertThat(true, is(true));
+    }
+}
